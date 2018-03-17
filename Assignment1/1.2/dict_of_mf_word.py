fe22c1a27b448a0671b544c79397f5f967dd12b9
Harish Thuwal <hthuwal@gmail.com>
2018-03-17 21:22:33 +0530

    Add file to gen vocabulory

---
+++ Assignment1/1.2/dict_of_mf_word.py
@@ -0,0 +1,39 @@
+import json
+import os
+import pickle
+import sys
+from sklearn.feature_extraction.text import CountVectorizer
+from tqdm import tqdm
+import multiprocessing as mp
+from collections import Counter
+from nltk.corpus import stopwords
+en_stop = set(stopwords.words('english'))
+
+# if not os.path.exists("models/mf_word_dict"):
+print("\nReading Data\n")
+data = []
+with open("../dataset/audio_train.json", "r") as f:
+    for line in tqdm(f):
+        data.append(json.loads(line))
+
+print("\nExtracting x's and y's\n")
+corpus = [(each["summary"]+" ")*4 + each["reviewText"] for each in data]
+y = [each["overall"] for each in data]
+
+vocab = []
+for each in tqdm(corpus):
+	temp = each.lower().split()
+	vocab += [ i for i in temp if i not in en_stop ]
+
+vocab = Counter(vocab)
+
+vocab = vocab.most_common()
+
+vocab = vocab[0:15000]
+
+word2idx = Counter()
+
+for i,w in enumerate(vocab):
+	word2idx[w[0]] = i+1
+
+pickle.dump(word2idx, open("word2idx","wb"))
