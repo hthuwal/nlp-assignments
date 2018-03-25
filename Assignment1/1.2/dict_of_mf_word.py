0fc4f00ac919aabbda8fcda95abef33e0ed745e4
Harish Thuwal <hthuwal@gmail.com>
2018-03-25 12:32:15 +0530

    Remove Redundant imports and pep8

--- Assignment1/1.2/dict_of_mf_word.py
+++ Assignment1/1.2/dict_of_mf_word.py
@@ -1,10 +1,6 @@
 import json
-import os
 import pickle
-import sys
-from sklearn.feature_extraction.text import CountVectorizer
 from tqdm import tqdm
-import multiprocessing as mp
 from collections import Counter
 from nltk.corpus import stopwords
 en_stop = set(stopwords.words('english'))
@@ -17,13 +13,14 @@ with open("../dataset/audio_train.json", "r") as f:
         data.append(json.loads(line))
 
 print("\nExtracting x's and y's\n")
-corpus = [(each["summary"]+" ")*4 + each["reviewText"] for each in data]
+corpus = [(each["summary"] + " ") * 4 + each["reviewText"] for each in data]
 y = [each["overall"] for each in data]
 
 vocab = []
 for each in tqdm(corpus):
-	temp = each.lower().split()
-	vocab += [ i for i in temp if i not in en_stop ]
+    temp = each.lower().split()
+    vocab += [i for i in temp if i not in en_stop]
+
 
 vocab = Counter(vocab)
 
@@ -33,7 +30,7 @@ vocab = vocab[0:15000]
 
 word2idx = Counter()
 
-for i,w in enumerate(vocab):
-	word2idx[w[0]] = i+1
+for i, w in enumerate(vocab):
+    word2idx[w[0]] = i + 1
 
-pickle.dump(word2idx, open("word2idx","wb"))
+pickle.dump(word2idx, open("word2idx", "wb"))
