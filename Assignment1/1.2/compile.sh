01b7fe0a7d4a1752a700e4ecb8fb1068bdddbfe4
Harish Thuwal <hthuwal@gmail.com>
2018-05-15 13:16:01 +0530

    run and compile scripts

---
+++ Assignment1/1.2/compile.sh
@@ -0,0 +1,8 @@
+if [ ! -f "2017MCS2074.cnn" ]
+then
+	wget -O 2017MCS2074.cnn "https://owncloud.iitd.ac.in/owncloud/index.php/s/tgToZKw9PHQMWgX/download"
+fi
+if [ ! -f "2017MCS2074.vocab" ]
+then	
+	wget -O 2017MCS2074.vocab "https://owncloud.iitd.ac.in/owncloud/index.php/s/ewdHBYbs9osNLz4/download"
+fi
